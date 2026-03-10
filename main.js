20e83f4162b55ba3a777b3c2fba63bed37114cc7
Pavlo12112 <pavlo280621@gmail.com>
2026-03-10 02:59:55 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,14 +1,16 @@
 const { URL } = require('url');
 
-const currency = "USD";
+const currency = "usd";
 const date_from = "20220707";
 const date_to = "20220719";
 
-const url = new URL("https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange");
+const url = new URL("https://bank.gov.ua/NBU_Exchange/exchange_site");
 
-url.searchParams.append("valcode", currency);
 url.searchParams.append("start", date_from);
 url.searchParams.append("end", date_to);
+url.searchParams.append("valcode", currency);
+url.searchParams.append("sort", "exchangedate");
+url.searchParams.append("order", "asc");
 url.searchParams.append("json", "");
 
 console.log(url.href);
